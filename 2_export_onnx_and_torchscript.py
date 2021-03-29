c3dfb83f3c6b27015f860f8afd058073e8671202
James Howard <github@jph.am>
2021-03-29 17:45:00 +0100

    updated export

--- 2_export_onnx_and_torchscript.py
+++ 2_export_onnx_and_torchscript.py
@@ -2,8 +2,7 @@ import torch.onnx
 from lib.models import *
 from lib.data import LABELS
 
-WEIGHTS_PATH = r"G:\APAYN\models\008\78_0.79991.pt"
-DEVICE = "cuda"
+WEIGHTS_PATH = r"E:\APAYN\models\008\78_0.79991.pt"
 INPUT_DIM = (1, 1, 576, 576)
 
 model = UneXt50(n_inputs=1, n_outputs=len(LABELS)).to(DEVICE)
@@ -26,15 +25,10 @@ torch.onnx.export(model,
                                            2: 'height',
                                            3: 'width'}})
 
-traced_model = torch.jit.trace(model, dummy_input)
-y = traced_model(dummy_input)
-print(traced_model.graph)
-traced_model.save(f"{WEIGHTS_PATH}_{DEVICE}.ts")
-
-DEVICE = "cpu"
-model = model.to(DEVICE)
-dummy_input = dummy_input.to(DEVICE)
-traced_model = torch.jit.trace(model, dummy_input)
-y = traced_model(dummy_input)
-print(traced_model.graph)
-traced_model.save(f"{WEIGHTS_PATH}_{DEVICE}.ts")
+for DEVICE in ('cpu', 'cuda'):
+    model = model.to(DEVICE)
+    dummy_input = dummy_input.to(DEVICE)
+    traced_model = torch.jit.trace(model, dummy_input)
+    _ = traced_model(dummy_input)
+    # print(traced_model.graph)
+    traced_model.save(f"{WEIGHTS_PATH}_{DEVICE}.ts")
